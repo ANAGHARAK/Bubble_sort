a9cc1bb2eb9a15fda158fffb287f62a1211f3ef4
ANAGHARAK <anukulkarni1110@gmail.com>
2022-07-16 18:58:02 +0530

    final code

--- Bubblesort.c
+++ Bubblesort.c
@@ -1,6 +1,30 @@
 #include<stdio.h>
 int main()
 {
-    int arr[5]={10,34,5,6,}
-    
+    int arr[5]={10,34,5,6,3};
+    int i,j,temp;
+    for(i=0;i<5;i++)
+    {
+        printf("%d\t",&arr[i]);
+    }
+    for(i=0;i<4;i++)
+    {
+        for(j=0;j<5-i;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+
+
+    }
+    printf("After sorting in ascending order, the array is\n");
+    for(i=0;i<5;i++)
+    {
+        printf("%d\t",&arr[i]);
+    }
+    return 0;    
 }
